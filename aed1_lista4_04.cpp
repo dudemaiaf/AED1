ac87e01bdb7657eefec3193a9108a73c6de6ad56
Mateus Souza <mota.mateus@live.com>
2017-04-05 13:50:30 -0400

    Add files via upload

--- aed1_lista4_04.cpp
+++ aed1_lista4_04.cpp
@@ -1,39 +1,32 @@
 #include <iostream>
 #define TAM 10
+#define posInicial 0
 #define NIL -1
 
 using namespace std;
 
-int buscaBinaria(int chave, int vetor[], int i, int j) {
-	if(i <= j){
-		int m = (i + j)/2;
-		if(chave == vetor[m]){
-			return m;
-		}if(chave < vetor[m]){
-			return buscaBinaria(chave, vetor, i, m-1);
-		}else{
-			return buscaBinaria(chave, vetor, m + 1, j);
-		}
+int buscaSequencial(int inicio, int vetor[], int chave){
+	if((inicio != (TAM-1)) && (vetor[inicio] == chave)){
+		return inicio;
+	}if((inicio != (TAM-1)) && (vetor[inicio] != chave)){
+		return buscaSequencial(inicio + 1, vetor, chave);
 	}else{
 		return NIL;
 	}
-	return 0;
 }
 
-void validarBusca(int chave, int vetor[], int i, int j){
-	if(buscaBinaria(chave, vetor, i, j) != NIL){
-		cout << "\nVetor[" << buscaBinaria(chave, vetor, i, j) << "] = " << chave << endl;
+void validarBusca(int inicio, int vetor[], int chave){
+	if(buscaSequencial(inicio, vetor, chave) != NIL){
+		cout << "\nVetor[" << buscaSequencial(inicio, vetor, chave) << "] = " << chave << endl;
 	}else{
-		cout << "\nNao houve exito na busca" << endl;
+		cout << "\nElemento nao encontrado" << endl;
 	}
 }
 
 int main(){
-	int vetor[TAM] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
-	int chave = 0;
-	int i = 0;
-	int j = TAM-1;
-	validarBusca(chave, vetor, i, j);
-
+	int vetor[TAM] = {0, 1, 4, 3, 10, 40, 50, 20, 34, 65};
+	int chave = 40;
+	validarBusca(posInicial, vetor, chave);
 	return 0;
 }
+
