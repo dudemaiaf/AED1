1b0a7bd908097417e24155622297bc1fa058bf90
MMateuSouza <mota.mateus@live.com>
2017-05-26 15:03:25 -0400

    Adicionando mais partes do projeto

--- ContarEntrada.cpp
+++ ContarEntrada.cpp
@@ -1,17 +1,38 @@
 #include <iostream>
-
+#define Limite 1000
 using namespace std;
 
 int main() {
   int contador = 0;
-  int fim = 0;
   string entrada;
+  string vetor[Limite];
   while(getline(cin, entrada)){
     if(entrada == "~"){
       break;
     }
+    vetor[contador] = entrada;
     contador ++;
   }
   cout << contador << endl;
+  for(int i = 0; i < contador+1; i ++){
+    cout << vetor[i] << "_";
+  }
   return 0;
 }
+
+/*
+A :
+    print 1
+    print 2
+    print 3
+
+B :
+    A
+    print 4
+    print 5
+    print 6
+
+Z :
+    B
+~
+*/
---
+++ LE.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+class LE{
+private:
+  string *vetor;
+  int tamanho;
+  int ultimo;
+public:
+  LE(int tamanho) {
+    this->tamanho = tamanho+1;
+    vetor = new string[this->tamanho];
+    ultimo = 0;
+  }
+
+  void insere(string Chave) {
+    if(ultimo < tamanho){
+      ultimo ++;
+      vetor[ultimo] = Chave;
+    }else{
+      cout << "\nLista cheia! \n";
+    }
+  }
+
+  void mostra() {
+    for(int i = 0; i <= ultimo; i++) {
+      cout << vetor[i] << " ";
+    }
+    cout << endl;
+  }
+
+  string busca(string Chave){
+    vetor[0] = Chave;
+    int i = ultimo;
+    while(Chave != vetor[i]) {
+      i --;
+    }if(i > 0){
+      return vetor[i];
+    }else{
+      return "";
+    }
+  }
+
+};
