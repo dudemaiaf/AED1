ec90057039b27d558e8ad854b894ba707d137640
Mateus Souza <mota.mateus@live.com>
2017-06-05 23:03:49 -0400

    Adicionando template e implementando métodos fora da classe

--- LinguagemNI.cpp
+++ LinguagemNI.cpp
@@ -29,29 +29,6 @@ public:
   void inserir(T Chave);
 };
 
-int LE::getLimite() {
-  return limite;
-}
-
-int LE::getUltimaPosicao() {
-  return ultimaPosicao;
-}
-
-template <class T>
-T LE::getELemento(int posicaoEscolhida){
-  return array[posicaoEscolhida];
-}
-
-template <class T>
-void LE::inserir(T Chave) {
-  if(ultimaPosicao < limite){
-    ultimaPosicao ++;
-    array[ultimaPosicao] = Chave;
-  }else{
-    cout << "\nLista cheia! \n";
-  }
-}
-
 template <class T>
 class No {
 private:
@@ -66,22 +43,6 @@ public:
   void setNo(No <T> *prox);
   No* getNo();
 };
-
-void No::setChave(T chave){
-  this->chave = chave;
-}
-
-T No::getChave() {
-  return chave;
-}
-
-void No::setNo(No <T> *prox) {
-  this->prox = prox;
-}
-
-No* No::getNo() {
-  return prox;
-}
 
 template <class T>
 class Pilha{
@@ -98,30 +59,6 @@ public:
 	void mostrar();
   bool vazia();
 };
-
-void Pilha::empilhar(T chave) {
-  No <T> *aux = new No<T>();
-  topo->setChave(chave);
-  aux->setNo(topo);
-  topo = aux;
-}
-
-void Pilha::desempilhar(T &chave) {
-  No <T> *aux = topo;
-  topo = topo->getNo();
-  chave = topo->getChave();
-  delete aux;
-}
-
-void Pilha::mostrar() {
-  for(No <T> *nav = topo->getNo(); nav != NULL; nav = nav->getNo()){
-    cout << nav->getString() << endl;
-  }
-}
-
-bool Pilha::vazia() {
-  return topo == fundo;
-}
 
 template <class T>
 class Ni{
@@ -144,21 +81,116 @@ public:
   void imprimirResultado();
 };
 
-bool Ni::verificarPrint(string linhaExecucao) {
+int main() {
+  LE <string> entradaPrograma(TAM);
+  Pilha <string> pilhaExecucao;
+  Ni <string> linguagemNi(entradaPrograma, pilhaExecucao);
+  linguagemNi.preencherEntradaPrograma();
+  linguagemNi.empilharMain();
+  linguagemNi.imprimirResultado();
+  return 0;
+}
+
+// Métodos da classe LE
+
+template <class T>
+int LE<T>::getLimite() {
+  return limite;
+}
+
+template <class T>
+int LE<T>::getUltimaPosicao() {
+  return ultimaPosicao;
+}
+
+template <class T>
+T LE<T>::getELemento(int posicaoEscolhida){
+  return array[posicaoEscolhida];
+}
+
+template <class T>
+void LE<T>::inserir(T Chave) {
+  if(ultimaPosicao < limite){
+    ultimaPosicao ++;
+    array[ultimaPosicao] = Chave;
+  }else{
+    cout << "\nLista cheia! \n";
+  }
+}
+
+// Métodos da classe No
+
+template <class T>
+void No<T>::setChave(T chave){
+  this->chave = chave;
+}
+
+template <class T>
+T No<T>::getChave() {
+  return chave;
+}
+
+template <class T>
+void No<T>::setNo(No <T> *prox) {
+  this->prox = prox;
+}
+
+template <class T>
+No<T>* No<T>::getNo() {
+  return prox;
+}
+
+// Métodos da classe Pilha
+
+template <class T>
+void Pilha<T>::empilhar(T chave) {
+  No <T> *aux = new No<T>();
+  topo->setChave(chave);
+  aux->setNo(topo);
+  topo = aux;
+}
+
+template <class T>
+void Pilha<T>::desempilhar(T &chave) {
+  No <T> *aux = topo;
+  topo = topo->getNo();
+  chave = topo->getChave();
+  delete aux;
+}
+
+template <class T>
+void Pilha<T>::mostrar() {
+  for(No <T> *nav = topo->getNo(); nav != NULL; nav = nav->getNo()){
+    cout << nav->getString() << endl;
+  }
+}
+
+template <class T>
+bool Pilha<T>::vazia() {
+  return topo == fundo;
+}
+
+// Métodos da classe Ni
+
+template <class T>
+bool Ni<T>::verificarPrint(string linhaExecucao) {
   return linhaExecucao[4] == 'P' && linhaExecucao[5] == 'R' && linhaExecucao[6] == 'I' && linhaExecucao[7] == 'N' && linhaExecucao[8] == 'T';
 }
 
-bool Ni::verificarChamadaFuncao(string linhaExecucao) {
+template <class T>
+bool Ni<T>::verificarChamadaFuncao(string linhaExecucao) {
   return linhaExecucao[4] == 'A' || linhaExecucao[4] == 'B' || linhaExecucao[4] == 'C' || linhaExecucao[4] == 'D' || linhaExecucao[4] == 'E' || linhaExecucao[4] == 'F' || linhaExecucao[4] == 'G' || linhaExecucao[4] == 'H' || linhaExecucao[4] == 'I' || linhaExecucao[4] == 'J'
          || linhaExecucao[4] == 'K' || linhaExecucao[4] == 'L' || linhaExecucao[4] == 'M' || linhaExecucao[4] == 'N' || linhaExecucao[4] == 'O' || linhaExecucao[4] == 'P' || linhaExecucao[4] == 'Q' || linhaExecucao[4] == 'R' || linhaExecucao[4] == 'S' || linhaExecucao[4] == 'T'
          || linhaExecucao[4] == 'U' || linhaExecucao[4] == 'V' || linhaExecucao[4] == 'X' || linhaExecucao[4] == 'W' || linhaExecucao[4] == 'Y' || linhaExecucao[4] == 'Z';
 }
 
-LE Ni::getEntradaPrograma() {
+template <class T>
+LE<T> Ni<T>::getEntradaPrograma() {
   return entradaPrograma;
 }
 
-void Ni::preencherEntradaPrograma() {
+template <class T>
+void Ni<T>::preencherEntradaPrograma() {
   string linhaLida;
   while(getline(cin, linhaLida)){
     if(linhaLida == "~"){
@@ -172,7 +204,8 @@ void Ni::preencherEntradaPrograma() {
   }
 }
 
-void Ni::empilharMain() {
+template <class T>
+void Ni<T>::empilharMain() {
   int flag = 0;
   int ultimaLinhaMain;
   for(int i = 1; i <= entradaPrograma.getUltimaPosicao(); i ++){
@@ -219,16 +252,7 @@ void Ni::empilharMain() {
   }
 }
 
-void Ni::imprimirResultado() {
+template <class T>
+void Ni<T>::imprimirResultado() {
   cout << resultado;
-}
-
-int main() {
-  LE <string> entradaPrograma(TAM);
-  Pilha <string> pilhaExecucao;
-  Ni <string> linguagemNi(entradaPrograma, pilhaExecucao);
-  linguagemNi.preencherEntradaPrograma();
-  linguagemNi.empilharMain();
-  linguagemNi.imprimirResultado();
-  return 0;
 }
